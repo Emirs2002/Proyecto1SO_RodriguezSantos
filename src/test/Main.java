0d993190dd7c0bc26e5192f466b9ce928b2ac8e6
alejanndrasan <asantos@correo.unimet.edu.ve>
2023-10-03 19:52:16 -0400

    'basesDone2'

--- src/test/Main.java
+++ src/test/Main.java
@@ -5,7 +5,6 @@ import interfaz.Pantalla;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello Isaac");
-        Pantalla pantalla1 = new Pantalla();
+        
     }
 }
